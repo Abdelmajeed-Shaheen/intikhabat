a85f4f73513d485e45a5570f20b4995388488005
yahyaalbashar <v.salvatore7.gs@gmail.com>
2020-07-29 23:51:30 +0300

    home page and staticfiles

--- campaign_manager/settings.py
+++ campaign_manager/settings.py
@@ -126,3 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOCAL_STATIC_CDN=os.path.join(os.path.dirname(BASE_DIR),'static_cdn')
+STATIC_ROOT=os.path.join(LOCAL_STATIC_CDN,'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'staticfiles')
+]
+
+MEDIA_ROOT=os.path.join(LOCAL_STATIC_CDN,'media')
+MEDIA_URL='/media/'
--- campaign_manager/urls.py
+++ campaign_manager/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from common.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",home,name='home'),
 ]
