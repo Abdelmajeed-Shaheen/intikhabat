eaaa818412993959115066676ee769774a093619
yahyaalbashar <v.salvatore7.gs@gmail.com>
2020-08-14 17:11:02 +0300

    reports

--- adminstration/urls.py
+++ adminstration/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views import (
                     CampaignManagementMainView,CreateComitteeView,
                     GrantPermissions,ComitteeMemberView,SearchComitteeView,
-                    CreateAreaView,CreateDistrictView                                                                                                                                                      
+                    CreateAreaView,CreateDistrictView,html_to_pdf_view,
+                    GetReportByCommitteeMember,GetGovernorateDepartment ,
+                    GetDepartmentArea,GetVotersList,by_committee_member_report,
+                    GetReportByIdentifier,by_identifier_report                                                                                                                                                 
                     )
 urlpatterns = [
     path("main",CampaignManagementMainView.as_view(),name="main"),
@@ -12,5 +15,15 @@ urlpatterns = [
     path("search-comittee",SearchComitteeView.as_view(),name="search-comittee"),
     path("create-area",CreateAreaView.as_view(),name="create-area"),
     path("create-district",CreateDistrictView.as_view(),name="create-district"),
+    path("pdf-report",html_to_pdf_view,name="pdf-report"),
+    path("cm-report",GetReportByCommitteeMember.as_view(),name="cm-report"),
+    path("get_gover_dept",GetGovernorateDepartment.as_view(),name="get_gover_dept"),
+    path("get_dept_area",GetDepartmentArea.as_view(),name="get_dept_area"),
+    path("get_voters",GetVotersList.as_view(),name="get_voters"),
+    path("by-cm-report",by_committee_member_report,name="by-cm-report"),
+    path("identifier-report",GetReportByIdentifier.as_view(),name="identifier-report"),
+    path("by-idn-report",by_identifier_report,name="by-idn-report"),
+
+
 
 ]
--- adminstration/views.py
+++ adminstration/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-from django.views.generic import CreateView,View
+from django.views.generic import CreateView,View,ListView
+from django.db.models import Q
 from .models import Comittee
 from users_management.models import ( 
                                         Candidate,CustomComitteePermission,
-                                        CustomMembersPermissions,ComitteeMember
+                                        CustomMembersPermissions,ComitteeMember,
+                                        Voter
                                     )
 from common.models import ( Address,
                             Department,
@@ -15,7 +17,13 @@ from common.models import ( Address,
 from .forms import CreateComitteeForm
 from users_management.forms import SignUpForm
 import json
+# testing purpose
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from weasyprint import HTML,CSS
 
+import json
 
 
 class CampaignManagementMainView(View):    
@@ -35,7 +43,6 @@ class CampaignManagementMainView(View):
         areas_list=Area.objects.all()
         districts_list=District.objects.all()
             
-
         context={
             "form":CreateComitteeForm,
             "comittee_member_form":SignUpForm,
@@ -132,13 +139,7 @@ class GrantPermissions(View):
         return JsonResponse({"message":"success"})
 
 
-class SearchComitteeView(View):
 
-    def get(self,request):
-        query=request.GET.get('query')
-        print(query)
-
-        return JsonResponse({"message":"success"})
 
 class CreateAreaView(View):
 
@@ -173,4 +174,279 @@ class CreateDistrictView(View):
         address.save()
 
         return JsonResponse({"message":"success"})
+
+def html_to_pdf_view(request):
+    paragraphs = ['يحى', 'صلاح', 'البشار']
+    html_string = render_to_string('report.html', {'paragraphs': paragraphs})
+    html = HTML(string=html_string,base_url=request.build_absolute_uri())
+    css=CSS('/home/yahya/Dev/dev_jpems/intikhabat/common/static/common/vendor/bootstrap/css/bootstrap.css')
+    html.write_pdf(target='/tmp/mypdf.pdf',stylesheets=[css])
+
+    fs = FileSystemStorage('/tmp')
+    with fs.open('mypdf.pdf') as pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+    return response
+
+
+
+class GetReportByCommitteeMember(ListView):
+    template_name="cm_report.html"
+    def get(self,request):
+        governorates_list=Governorate.objects.all()
+
+        if hasattr(request.user.userprofile,'campaignadminstrator'):
+            candidate=request.user.userprofile.campaignadminstrator.candidate
         
+        elif hasattr(request.user.userprofile,'comitteemember'):
+            candidate=request.user.userprofile.comitteemember.candidate
+
+        else :
+            candidate=request.user.userprofile.candidate
+
+        
+        comittee_members_list=ComitteeMember.objects.filter(candidate=candidate)
+        context={
+            'gover_list':governorates_list,
+            'cm_members_list':comittee_members_list
+            
+        }
+        
+        return render(request,self.template_name,context)
+
+
+class GetGovernorateDepartment(View):
+
+    def get(self,request):
+        response=[]
+        gover_id=request.GET.get('gover_id')
+        governorate=Governorate.objects.get(id=int(gover_id))
+        dept_list=Department.objects.filter(governorate=governorate)
+
+        for dept in dept_list:
+            item={}
+            item['name']=dept.name
+            item['id']=dept.id
+            response.append(item)
+        
+        return JsonResponse(response,safe=False)
+
+class GetDepartmentArea(View):
+
+    def get(self,request):
+        response=[]
+        dept_id=request.GET.get('dept_id')
+        dept=Department.objects.get(id=int(dept_id))
+        areas_list=Area.objects.filter(department=dept)
+
+        for area in areas_list:
+            item={}
+            item['name']=area.name
+            item['id']=area.id
+            response.append(item)
+        
+        return JsonResponse(response,safe=False)
+
+class SearchComitteeView(View):
+
+    def get(self,request): 
+        query=request.GET.get('query')
+        print(query)
+
+        return JsonResponse({"message":"success"})
+
+
+class GetVotersList(View):
+
+    def get(self,request):
+        
+        response=[]
+        query=request.GET.get('query')
+        query=json.loads(query)
+        search_object={}
+  
+        if query['cm_id'] not in [None,""]:
+            cm=ComitteeMember.objects.get(id=int(query['cm_id']))
+            search_object['profile__address__district']=cm.profile.address.district
+            search_object['candidate']=cm.candidate
+
+        if query['is_identifier']:
+                identifier_object={
+                    'is_identifier':True
+                }
+                if query['identifier_name'] not in [None,""]:
+
+                    identifier_name=query['identifier_name']
+                    identifier_name=identifier_name.replace(" ","")
+                    identifier_object["profile__name_string"]=identifier_name
+                
+                if  query['identifier_wa'] not in [None,""]:
+                    identifier_object['profile__whatsapp_number']=query['identifier_wa']
+
+                identifier=Voter.objects.get(**identifier_object,candidate=cm.candidate)
+                search_object["identiefier"]=identifier
+                
+                if  query['identifier_mobile'] not in [None,""]:
+                    identifier_object['profile__mobile_number']=query['identifier_mobile']            
+
+
+        if query['area_id'] not in [None,""]:
+            area=Area.objects.get(id=int(query['area_id']))
+            search_object['profile__address__area']=area
+
+        if query['gover_id'] not in [None,""]:
+            governorate=Governorate.objects.get(id=int(query['gover_id']))
+            search_object['profile__address__governorate']=governorate
+
+        if query['dept_id'] not in [None,""]:
+            dept=Department.objects.get(id=int(query['dept_id']))
+            search_object['profile__address__department']=dept
+
+        voters_list=Voter.objects.filter(**search_object)        
+        for voter in voters_list:
+            obj={}
+            obj['id']=voter.id
+            obj['full_name']=voter.profile.user.first_name
+            obj['mobile_no']=voter.profile.mobile_number
+            if voter.identiefier:
+                obj['identifier']=(voter.identiefier.profile.user.first_name+" "+voter.identiefier.profile.user.last_name)
+                obj['identifier_phone_no']=voter.identiefier.profile.mobile_number
+                obj['identifier_whats_no']=voter.identiefier.profile.whatsapp_number
+            else:
+                obj['identifier']='لا يوجد'
+                obj['identifier_phone_no']='لا يوجد'
+                obj['identifier_whats_no']='لا يوجد'
+
+            if voter.has_elc_card:
+                obj['has_elc_card']='نعم'
+            else:
+                obj['has_elc_card']='لا'
+           
+                
+            response.append(obj)
+        if identifier is not None:
+            identifier_response={}
+            identifier_response['idn_name']=(identifier.profile.user.first_name+" "+identifier.profile.middle_name+" "+identifier.profile.last_name+" "+identifier.profile.user.last_name)
+            
+        data={
+            'data':response,
+            "identifier":identifier_response
+        }
+        return JsonResponse(data,safe=False)
+
+
+def by_committee_member_report(request):
+
+    query=request.GET.get('query')
+    query=json.loads(query)
+    search_object={}
+    if query['cm_id'] not in [None,""]:
+        cm=ComitteeMember.objects.get(id=int(query['cm_id']))
+        search_object['profile__address__district']=cm.profile.address.district
+        search_object['candidate']=cm.candidate
+        
+    if query['area_id'] not in [None,""]:
+        area=Area.objects.get(id=int(query['area_id']))
+        search_object['profile__address__area']=area
+
+    if query['gover_id'] not in [None,""]:
+        governorate=Governorate.objects.get(id=int(query['gover_id']))
+        search_object['profile__address__governorate']=governorate
+
+    if query['dept_id'] not in [None,""]:
+        dept=Department.objects.get(id=int(query['dept_id']))
+        search_object['profile__address__department']=dept
+    
+    voters_list=Voter.objects.filter(**search_object)
+    
+    html_string = render_to_string('by_cm_report.html', {'voters_list': voters_list})
+    html = HTML(string=html_string,base_url=request.build_absolute_uri())
+    css=CSS('/home/yahya/Dev/dev_jpems/intikhabat/common/static/common/vendor/bootstrap/css/bootstrap.css')
+    html.write_pdf(target='/tmp/mypdf.pdf',stylesheets=[css])
+    fs = FileSystemStorage('/tmp')
+    with fs.open('mypdf.pdf') as pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+    return response
+
+
+
+class GetReportByIdentifier(View):
+    template_name="identifier_report.html"
+    def get(self,request):
+        governorates_list=Governorate.objects.all()
+
+        if hasattr(request.user.userprofile,'campaignadminstrator'):
+            candidate=request.user.userprofile.campaignadminstrator.candidate
+        
+        elif hasattr(request.user.userprofile,'comitteemember'):
+            candidate=request.user.userprofile.comitteemember.candidate
+
+        else :
+            candidate=request.user.userprofile.candidate
+        comittee_members_list=ComitteeMember.objects.filter(candidate=candidate)
+
+
+        context={
+            'gover_list':governorates_list,
+            'cm_members_list':comittee_members_list
+            
+        }
+        
+        return render(request,self.template_name,context)
+
+
+def by_identifier_report(request):
+
+    query=request.GET.get('query')
+    query=json.loads(query)
+    search_object={}
+    display_idn_name=""
+    if query['cm_id'] not in [None,""]:
+        cm=ComitteeMember.objects.get(id=int(query['cm_id']))
+        search_object['profile__address__district']=cm.profile.address.district
+        search_object['candidate']=cm.candidate
+
+    if query['is_identifier']:
+            identifier_object={
+                'is_identifier':True
+            }
+            if query['identifier_name'] not in [None,""]:
+                
+                identifier_name=query['identifier_name']
+                name=identifier_name.replace(" ","")
+                identifier_object["profile__name_string"]=name
+            
+            if  query['identifier_wa'] not in [None,""]:
+                identifier_object['profile__whatsapp_number']=query['identifier_wa']
+
+            identifier=Voter.objects.get(**identifier_object,candidate=cm.candidate)
+            display_idn_name=identifier
+            search_object["identiefier"]=identifier
+            
+            if  query['identifier_mobile'] not in [None,""]:
+                identifier_object['profile__mobile_number']=query['identifier_mobile']            
+
+
+    if query['area_id'] not in [None,""]:
+        area=Area.objects.get(id=int(query['area_id']))
+        search_object['profile__address__area']=area
+
+    if query['gover_id'] not in [None,""]:
+        governorate=Governorate.objects.get(id=int(query['gover_id']))
+        search_object['profile__address__governorate']=governorate
+
+    if query['dept_id'] not in [None,""]:
+        dept=Department.objects.get(id=int(query['dept_id']))
+        search_object['profile__address__department']=dept
+    voters_list=Voter.objects.filter(**search_object)
+    
+    html_string = render_to_string('by_idn_report.html', {'voters_list': voters_list,'display_idn_name':display_idn_name})
+    html = HTML(string=html_string,base_url=request.build_absolute_uri())
+    css=CSS('/home/yahya/Dev/dev_jpems/intikhabat/common/static/common/vendor/bootstrap/css/bootstrap.css')
+    html.write_pdf(target='/tmp/by_identifier_report.pdf',stylesheets=[css])
+    fs = FileSystemStorage('/tmp')
+    with fs.open('by_identifier_report.pdf') as pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="by_identifier_report.pdf"'
+    return response
