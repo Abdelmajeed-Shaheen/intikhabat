8996a4d67cad1de005a929f88d4dc1f3bed98c8f
yahyaalbashar <v.salvatore7.gs@gmail.com>
2020-08-18 13:50:05 +0300

    select candidate

--- voters_management/urls.py
+++ voters_management/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,re_path
-from .views import CreateVoter,VoterProfile,UpdateVoter
+from .views import CreateVoter,VoterProfile,UpdateVoter,GetCandidates
 urlpatterns = [
     path('create-voter/',CreateVoter.as_view(),name="create-voter"),
     re_path(r'^(?P<pk>[0-9]+)/$',VoterProfile.as_view(),name="voter-profile"),
-    path("edit-voting-status/",UpdateVoter.as_view(),name="edit-voting-status")
+    path("edit-voting-status/",UpdateVoter.as_view(),name="edit-voting-status"),
+    path("get_candidates/",GetCandidates.as_view(),name="get_candidates")
+
 ]
