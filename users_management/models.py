0f7c29caad498a72352bc6d95d9318902b87b704
yahyaalbashar <v.salvatore7.gs@gmail.com>
2020-08-19 11:42:10 +0300

    fixes

--- users_management/models.py
+++ users_management/models.py
@@ -62,7 +62,7 @@ class Candidate(models.Model):
     profile=models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     timestamp=models.DateTimeField(auto_now=True,auto_now_add=False)
     updated=models.DateTimeField(auto_now=False,auto_now_add=True)
-    election_list=models.OneToOneField(to='adminstration.ElectionList',on_delete=models.CASCADE,null=True)
+    election_list=models.ForeignKey(to='adminstration.ElectionList',on_delete=models.CASCADE,null=True)
     title=models.CharField(max_length=255,blank=True,null=True)
     class Meta:
         permissions = [
