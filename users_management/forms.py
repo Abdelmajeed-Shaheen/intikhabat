587a93a51a0620d44d264072174dd33665ed03bf
yahyaalbashar <v.salvatore7.gs@gmail.com>
2020-08-30 14:09:36 +0300

    validation in adminstration

--- users_management/forms.py
+++ users_management/forms.py
@@ -107,4 +107,64 @@ class SignUpForm(forms.Form):
     date_of_birth=forms.DateField()
     is_manager=forms.BooleanField(widget=forms.CheckboxInput(attrs={'class':'form-check-input'}))
 
+
+class CampaignAdminCreateForm(forms.Form):
+    ca_first_name = forms.CharField(
+                                 widget=forms.TextInput(
+                                        attrs={
+                                            "class": "form-control",
+                                            "placeholder": "الاسم الاول"
+                                            }
+                                        )
+    )
+    ca_last_name = forms.CharField(
+                                widget=forms.TextInput(
+                                        attrs={
+                                                "class": "form-control",
+                                                "placeholder": "الاسم العائلة"
+                                                }
+                                            )
+                                
+    )
+
+    ca_middle_name=forms.CharField(
+                                widget=forms.TextInput(
+                                        attrs={
+                                            "class": "form-control",
+                                            "placeholder": "اسم الاب"
+                                            }
+                                        )
+    )
+    ca_third_name=forms.CharField(
+                                widget=forms.TextInput(
+                                        attrs={
+                                            "class": "form-control",
+                                            "placeholder": "اسم الجد"
+                                            }
+                                        )
+    )
+    ca_mobile_number=forms.CharField(
+                                   widget=forms.TextInput(
+                                        attrs={
+                                            "class": "form-control",
+                                            "placeholder": "رقم الهاتف"
+                                            }
+                                        )
+    
+    )
+    ca_whatsapp_number=forms.CharField(
+                                     widget=forms.TextInput(
+                                        attrs={
+                                            "class": "form-control",
+                                            "placeholder": "رقم الواتساب"
+                                            }
+                                        )
+    )
+    ca_email=forms.EmailField( widget=forms.TextInput(
+                                        attrs={
+                                            "class": "form-control",
+                                            "placeholder": "البريد الالكتروني"
+                                            }
+                                        ))
+    ca_date_of_birth=forms.DateField()
     
